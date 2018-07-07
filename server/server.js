84b82b11a4afc8702560c1a3cd267ce8897ebc02
Tarun Singh <tarunsingh233461@gmail.com>
2018-07-07 22:28:18 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyparser=require('body-parser');
+var {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose');
 var {todo}=require('./models/todo');
@@ -19,6 +20,31 @@ app.post('/todos',(req,res)=>{
 	});
 });
 
+
+// app.get('/todos',(req,res)=>{
+// 	todo.find().then((todos)=>{
+// 		req.send(todos);
+// 	},(e)=>{
+// 			req.status(400).send(e);
+// 	});
+// });
+
+app.get('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	if(!ObjectID.isValid(id))
+	{
+		return res.status(404).send();
+	}
+	todo.findById(id).then((todo)=>{
+			if(!todo)
+			{
+				return res.status(404).send();
+			}
+			res.send({todo});
+	}).catch((e)=>{
+		res.status(400).send(e);
+	});
+});
 
 app.listen(3000,()=>{
 	console.log('started on port 3000');
